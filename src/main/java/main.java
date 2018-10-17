5b7f5da70f64c360295d70ce9b0388a4b68e0ec2
davidriquelmer <34255430+davidriquelmer@users.noreply.github.com>
2018-10-17 16:51:00 -0400

    Cambios ejercicio 1

---
+++ src/main/java/main.java
@@ -0,0 +1,28 @@
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+public class main {
+    public static void main(String[] args) throws Exception {
+        if (args.length != 2) {
+            System.err.println("Usage: TwitterKeyWorkDriver <input path> <output path>");
+            System.exit(-1);
+        }
+        Job job = new Job();
+        job.setJarByClass(main.class);
+        job.setJobName("Count TweetsbyUsr");
+
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+        job.setMapperClass(mapper.class);
+        job.setReducerClass(reducer.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+        System.out.println("Done!");
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }
+}
