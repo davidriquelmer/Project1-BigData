b1efbfb715dd20759e590626182dac44c1c1ee66
davidriquelmer <34255430+davidriquelmer@users.noreply.github.com>
2018-10-17 20:50:41 -0400

    Ejercicio2
    Count the number of different keywords on in the message other than stop words: a, then,
    the, in , out, , which, etc

--- Ejercicio1/src/main/java/main.java
+++ Ejercicio1/src/main/java/main.java
@@ -1,9 +1,9 @@
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.IntWritable;
-import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapreduce.Job;
-import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
-import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+        import org.apache.hadoop.io.IntWritable;
+        import org.apache.hadoop.io.Text;
+        import org.apache.hadoop.mapreduce.Job;
+        import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+        import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 public class main {
     public static void main(String[] args) throws Exception {
